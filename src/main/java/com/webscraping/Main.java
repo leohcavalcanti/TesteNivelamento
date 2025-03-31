13f01e2b3d2f547445fd61b6b36800b4f7f871e6
leohcavalcanti <leonardocaturyty@hotmail.com>
2025-03-31 00:55:28 -0300

    main search

--- src/main/java/com/webscraping/Main.java
+++ src/main/java/com/webscraping/Main.java
@@ -1,7 +1,17 @@
 package com.webscraping;
 
+import com.webscraping.controller.DocumentScraperController;
+import com.webscraping.service.DocumentScraperService;
+import com.webscraping.view.ConsoleView;
+
 public class Main {
     public static void main(String[] args) {
+        String targeUrl = "https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos";
 
+        DocumentScraperService service = new DocumentScraperService();
+        ConsoleView view = new ConsoleView();
+        DocumentScraperController controller = new DocumentScraperController(service, view);
+
+        controller.scrapeAndProcess(targeUrl);
     }
 }
